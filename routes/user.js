8ea713b0685d4960f95f7728ace7fcc81a06b48a
Wes Duff <slysop@gmail.com>
2013-10-31 18:04:56 -0500

    adding angular and services

--- routes/user.js
+++ routes/user.js
@@ -1,37 +1,42 @@
-var mongoose = require( 'mongoose');
-var User = mongoose.model( 'User' );
+var mongoose = require('mongoose');
+var User = mongoose.model('User');
 
-// Get all users
-exports.index = function(req, res){
-	res.render('user-index', {
-		title: 'All Users will display here'
-	});
+// GET user creation form
+exports.create = function (req, res) {
+    res.render('user-form', {
+        title: 'Create user',
+        buttonText: "Join!"
+    });
 };
 
-// Get user creation form
-exports.create = function(req, res){
-	res.render('user-form', {
-		title: 'Create user',
-		buttonText: "Join!"
-	});
+exports.index = function (req, res) {
+    res.render('user-index', {
+        title: 'Users',
+        buttonText: "Hello"
+    });
 };
 
-// Post new user creation form
-exports.doCreate = function(req, res){
-	User.create({
-		name: req.body.FullName,
-		email: req.body.Email,
-		modifiedOn : Date.now(),
-		lastLogin: Date.now()
-	}, function( err, user ){
-		if(err){
-			console.log(err);
-			if(err.code===11000){
-				res.redirect('/user/new?exists=true');
-			}
-		} else {
-			//success
-			console.log("User created and saved: " + user);
-		}
-	});
+// POST new user creation form
+exports.doCreate = function (req, res) {
+    console.log(req);
+    User.create({
+        name: req.body.fullName,
+        email: req.body.email,
+        modifiedOn: req.body.modifiedOn,
+        lastLogin: req.body.lastLogin
+    }, function (err, user) {
+        if (err) {
+            console.log(err);
+            if (err.code === 11000) {
+                alert('error');
+                res.redirect('/user/new?exists=true');
+            } else {
+                res.redirect('/?error=true');
+            }
+        } else {
+            // Success
+            console.log("User cretated and savced: " + user);
+            res.redirect('/user/new?saved=true');
+        }
+    });
 };
